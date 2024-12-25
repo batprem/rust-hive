69ed7e3165dbd70977c9e8111ed0cdd236df3cc5
prem <prem.ch@ku.th>
2024-12-25 20:45:17 +0700

    refactor: Refactor and apply struct to code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 mod databases;
+mod experiment;
 mod parsers;
 use databases::duckdb_functions::{
-    create_duck_db_table, generate_insert_sql, write_into_hive_partition,
+    create_duck_db_table, generate_insert_sql_given_row_struct, write_into_hive_partition,
 };
-use duckdb::{Connection, Result};
-use std::error::Error;
+use duckdb::{Connection, Error as DuckDBError, Result};
+
+use reqwest::Error as RequestwestError;
+use rust_hive::parsers::population::PopulationRow;
+use thiserror::Error;
+
+// Custom error handling
+#[derive(Error, Debug)]
+enum IngestionError {
+    #[error("Error connecting to DuckDB: {0}")]
+    DuckDB(#[from] DuckDBError),
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Requestwest error: {0}")]
+    Requestwest(#[from] RequestwestError),
+    #[error("Parse error: {0}")]
+    Parse(String),
+}
 
 /// Converts a Gregorian year to a Thai year.
 ///3
@@ -22,76 +39,85 @@ fn convert_to_thai_year(year: i32) -> i32 {
     year + 543 - 2500
 }
 
-fn get_data_stat_by_year(year: i32) -> Result<String, Box<dyn Error>> {
+fn get_data_stat_by_year(year: i32) -> Result<String, String> {
     let thai_year = convert_to_thai_year(year);
     let url = format!(
         "https://stat.bora.dopa.go.th/new_stat/file/{}/stat_c{}.txt",
         thai_year, thai_year
     );
-    let response = reqwest::blocking::get(url)?;
-    if response.status().as_u16() != 200 {
-        return Err(format!("Not found: HTTP {}", response.status().as_u16()).into());
+    let mut result = String::new();
+    if let Ok(response) = reqwest::blocking::get(url) {
+        if response.status().as_u16()/ 200 != 2 {
+            return Err(format!(
+                "Fail request with HTTP code: {:?}",
+                response.status().as_u16()
+            ));
+        }
+        result = response.text().ok().unwrap();
     }
-    let result = response.text()?;
 
     Ok(result.trim_matches(|c| c == ' ' || c == '\n').to_string())
 }
 
-fn clean_text(text: &str) -> String {
-    text.trim_matches('\u{feff}').to_string()
+fn extract_row(row: &str) -> Vec<String> {
+    row.split('|')
+        .into_iter()
+        .map(|value| value.to_string())
+        .collect::<Vec<String>>()
 }
 
-fn extract_row(row: &str) -> Vec<&str> {
-    row.split('|').collect()
+/// Updates a row in the database with population data.
+///
+/// This function processes a line of population data, parses it into a `PopulationRow` struct,
+/// generates an SQL insert statement, and executes it against the provided database connection.
+/// Note: Duckdb has internal mechanism which supports ACID
+///
+/// # Parameters
+///
+/// * `conn` - A reference to a DuckDB `Connection` object for database operations.
+/// * `line` - A string slice containing the raw population data to be processed.
+/// * `year` - An integer representing the year of the population data.
+///
+/// # Returns
+///
+/// A `Result` which is:
+/// * `Ok` with a `String` "Updated population" if the operation was successful.
+/// * `Err` with a boxed dynamic `Error` if any step in the process fails.
+fn update_row(conn: &Connection, line: &str, year: i32) -> Result<String, IngestionError> {
+    // Extract fields from the line and convert them into a PopulationRow struct
+    let extracted = extract_row(line.trim_matches(|c| ['|', ' ', '\n', '\r'].contains(&c)));
+    let population_row = match PopulationRow::parse(extracted) {
+        Ok(row) => row,
+        Err(e) => return Err(IngestionError::Parse(e)),
+    };
+
+    // Generate an SQL insert statement and execute it against the database connection
+    let insert_sql = generate_insert_sql_given_row_struct(year, &population_row);
+    conn.execute(&insert_sql, [])?;
+
+    // Return success message
+    Ok("Updated population".to_string())
 }
 
-fn string_to_int(value: &str) -> Result<i32, std::num::ParseIntError> {
-    value.replace(",", "").parse::<i32>()
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    println!("Run main");
+fn main() -> Result<(), IngestionError> {
+    println!("Run ingestion");
     // Create a Duckdb table
     let conn = Connection::open_in_memory()?;
     create_duck_db_table(&conn)?;
 
     // Initial year
     let mut year = 1993;
-
+    // Ingest data and update the database in batches of 1000 rows per transactions
+    conn.execute("BEGIN TRANSACTION", [])?;
     while let Ok(data) = get_data_stat_by_year(year) {
-        for line in data.split("\n") {
-            // TODO: Make it to a function
-            let extracted = extract_row(line.trim_matches(|c| ['|', ' ', '\n', '\r'].contains(&c)));
-            if let [yymm, cc_code, cc_desc, rcode_code, rcode_desc, ccaatt_code, ccaatt_desc, ccaattmm_code, ccaattmm_desc, male_str, female_str, total_str, house_str] =
-                extracted[..]
-            {
-                let male = string_to_int(male_str)?;
-                let female = string_to_int(female_str)?;
-                let total = string_to_int(total_str)?;
-                let house = string_to_int(house_str)?;
-                let insert_sql = generate_insert_sql(
-                    year,
-                    yymm,
-                    cc_code.parse::<i32>()?,
-                    &clean_text(cc_desc),
-                    rcode_code,
-                    &clean_text(rcode_desc),
-                    ccaatt_code,
-                    &clean_text(ccaatt_desc),
-                    ccaattmm_code,
-                    &clean_text(ccaattmm_desc),
-                    male,
-                    female,
-                    total,
-                    house,
-                );
-                conn.execute(&insert_sql, [])?;
-            } else {
-                println!("Row does not have the correct number of fields");
-            }
-        }
+        let _: Vec<_> = data
+            .split("\n")
+            .into_iter()
+            .map(|line| update_row(&conn, line, year))
+            .collect();
         year += 1;
     }
+    conn.execute("COMMIT", [])?;
     write_into_hive_partition(&conn)?;
     Ok(())
 }
